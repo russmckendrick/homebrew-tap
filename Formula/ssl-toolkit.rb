e108bad253cb7d618fcccb6ca23af7328fbb9b1f
GitHub Action <action@github.com>
2026-02-01 09:39:14 +0000

    Update ssl-toolkit to v0.0.5

--- Formula/ssl-toolkit.rb
+++ Formula/ssl-toolkit.rb
@@ -6,10 +6,10 @@ class SslToolkit < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/russmckendrick/ssl-toolkit/releases/download/v0.0.5/ssl-toolkit-darwin-arm64"
-      sha256 "Not"
+      sha256 "9e362e94e67679d75a7c3b4e30b553d28d076ee72d041af29413044bf22cfae6"
     else
       url "https://github.com/russmckendrick/ssl-toolkit/releases/download/v0.0.5/ssl-toolkit-darwin-amd64"
-      sha256 "Not"
+      sha256 "8a0046bacf1c9edb4d69ef4bc50ece5236f3f50f389683982dbaaa692da5ac0b"
     end
   end
 
