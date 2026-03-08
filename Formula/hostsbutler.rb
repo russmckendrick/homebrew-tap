32b3f1cebed188e98b4295f6330bd2c4947a5819
GitHub Action <action@github.com>
2026-03-08 11:53:19 +0000

    Update hostsbutler to v0.0.3

--- Formula/hostsbutler.rb
+++ Formula/hostsbutler.rb
@@ -1,15 +1,15 @@
 class Hostsbutler < Formula
   desc "A cross-platform TUI application for managing the system hosts file"
   homepage "https://github.com/russmckendrick/hostsbutler"
-  version "0.0.2"
+  version "0.0.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/russmckendrick/hostsbutler/releases/download/v0.0.2/hostsbutler-darwin-arm64"
-      sha256 "8b51a2157810b422c95f310b07fa6343407787ff64b63d03066afccab04f1d4b"
+      url "https://github.com/russmckendrick/hostsbutler/releases/download/v0.0.3/hostsbutler-darwin-arm64"
+      sha256 "c7c61d01b6fdbcabd3e5ea5d01080662b0f618704035c7af22d386c68968ca22"
     else
-      url "https://github.com/russmckendrick/hostsbutler/releases/download/v0.0.2/hostsbutler-darwin-amd64"
-      sha256 "0351ba3a34a1f2ae9136749fcb0f7fd058f937d8ddb60cd985a0c09f0572c4fb"
+      url "https://github.com/russmckendrick/hostsbutler/releases/download/v0.0.3/hostsbutler-darwin-amd64"
+      sha256 "ba231ef7dcce4e92e898a3aefd5ba728851b3a56c821e02c63a656b7d0c2defa"
     end
   end
 
