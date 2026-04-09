1d0fc7fe1c46aee67af2c16e877371336f359e7b
GitHub Action <action@github.com>
2026-04-09 18:48:45 +0000

    Update aicommit to v0.0.4

--- Formula/aicommit.rb
+++ Formula/aicommit.rb
@@ -1,15 +1,15 @@
 class Aicommit < Formula
   desc "AI-assisted Git commit messages"
   homepage "https://github.com/russmckendrick/aicommit"
-  version "0.0.3"
+  version "0.0.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/russmckendrick/aicommit/releases/download/v0.0.3/aic-darwin-arm64"
-      sha256 "43b743bb2220eac7e5d650940124f1378083115ffbf397ac3b75df667b71b1eb"
+      url "https://github.com/russmckendrick/aicommit/releases/download/v0.0.4/aic-darwin-arm64"
+      sha256 "edc6b516c5f235ccfda417a43bdf629ee06334af0c1b305a1d1d422497bb9cfe"
     else
-      url "https://github.com/russmckendrick/aicommit/releases/download/v0.0.3/aic-darwin-amd64"
-      sha256 "f65abd492a256635d28104f633ea2f127ab8556c14632cc8ba3557c889d14951"
+      url "https://github.com/russmckendrick/aicommit/releases/download/v0.0.4/aic-darwin-amd64"
+      sha256 "bbe933c29a543bc19962e9e566bb7113a16dd56aa1db54a7c563b031eb9cb95c"
     end
   end
 
