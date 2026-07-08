6540961a90b5b6b180e2fdc1f28ad208c94e70ac
GitHub Action <action@github.com>
2026-07-08 18:12:50 +0000

    Update terminal-svg to v0.6.0

--- Formula/terminal-svg.rb
+++ Formula/terminal-svg.rb
@@ -1,25 +1,25 @@
 class TerminalSvg < Formula
   desc "Pixel-perfect SVG screenshots of terminal output with window chrome"
   homepage "https://github.com/russmckendrick/terminal-svg"
-  version "0.5.0"
+  version "0.6.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/russmckendrick/terminal-svg/releases/download/v0.5.0/terminal-svg-darwin-arm64"
-      sha256 "db6f901ccd2ee9666a37ca49425cd5939cf490310fe6a6bc4273cc2d7e32760f"
+      url "https://github.com/russmckendrick/terminal-svg/releases/download/v0.6.0/terminal-svg-darwin-arm64"
+      sha256 "22da0029cc3ad292c96563a16d90df3b2e63a572551e1d807368891ffa2d792b"
     else
-      url "https://github.com/russmckendrick/terminal-svg/releases/download/v0.5.0/terminal-svg-darwin-amd64"
-      sha256 "37ed4fa8174057bb9ba6117dbea5ed0f00e0d52adf8e501be7570caa0fbe8aab"
+      url "https://github.com/russmckendrick/terminal-svg/releases/download/v0.6.0/terminal-svg-darwin-amd64"
+      sha256 "0c6b1e68990a8fbba776c0c357611bf4979577e2fa9c337c41b574ffb540e59c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/russmckendrick/terminal-svg/releases/download/v0.5.0/terminal-svg-linux-arm64"
-      sha256 "b8018e96a0f37c21f43137bd675e2853c355ed91243c9f15f538f78b6cb13f86"
+      url "https://github.com/russmckendrick/terminal-svg/releases/download/v0.6.0/terminal-svg-linux-arm64"
+      sha256 "d04d0a74534b24e9c126494010beb06ca2a374f44fba4470b4a667acb032131d"
     else
-      url "https://github.com/russmckendrick/terminal-svg/releases/download/v0.5.0/terminal-svg-linux-amd64"
-      sha256 "c40f9a8b8734fba71de8f033d2ddf17f98d76b776b5aa1df5c9bfb5730f52e50"
+      url "https://github.com/russmckendrick/terminal-svg/releases/download/v0.6.0/terminal-svg-linux-amd64"
+      sha256 "d2143e660791d2be09fc051ba08ced099f7d1b864c90a2f1bf1dad52386b5faa"
     end
   end
 
